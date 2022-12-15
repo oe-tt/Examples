71ab74c4abb8fa360ac296eda64093ec362265d7
oe-tt <oe-tt@mail.ru>
2022-12-15 22:47:09 +0300

    div

---
+++ 004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿double numberA = new Random().Next(1,10); // 1 2 3 ... 9
+double numberB = new Random().Next(1,10);
+Console.WriteLine (numberA);
+Console.WriteLine (numberB);
+Console.WriteLine (numberA / numberB);
