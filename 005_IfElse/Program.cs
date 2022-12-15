59caf8c4c82283332049ccd9a170aadd492ddd27
oe-tt <oe-tt@mail.ru>
2022-12-15 22:47:49 +0300

    done

---
+++ 005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "вика")
+{
+    Console.WriteLine("Ура, это же Вика!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
